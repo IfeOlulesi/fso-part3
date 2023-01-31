4f71c60bf69d0edf8554058ea3719e0cd5aa2022
IfeOlulesi <ifeolulesi@gmail.com>
2023-01-31 14:34:29 +0100

    Tutorial examples follow along

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -25,6 +27,53 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  console.log(request.headers)
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    return response.json(note)
+  } else {
+    response.statusMessage = "Note not found"
+    return response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  console.log(request.headers)
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const idGenerator = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)): 0
+  return maxId + 1
+}
+
+// create new note
+app.post('/api/notes', (request, response) => {  
+  const body = request.body
+  
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    id: idGenerator(),
+    content: body.content,
+    important: body.important || false,
+    dateCreated: new Date(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001
