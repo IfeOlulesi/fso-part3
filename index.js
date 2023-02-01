aa62063ce79e5783954cdca2758721b6e708e75e
IfeOlulesi <ifeolulesi@gmail.com>
2023-02-01 15:48:00 +0100

    Middleware

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const app = express()
 const notesApi = require('./routes/notes')
 const personsApi = require('./routes/phonebook')
 
+const requestLogger = (req, res, next) => {
+  console.log("Method", req.method)
+  console.log("Path", req.path)
+  console.log("Body", req.body)
+  console.log('---')
+  next()
+}
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
 app.use(express.json())
+app.use(requestLogger)
 
 
 // notes app routes
@@ -29,6 +44,10 @@ app.get('/api/persons/:id', (req, res) => personsApi.getContactById(req, res))
 app.delete('/api/persons/:id', (req, res) => personsApi.deleteContact(req, res))
 
 app.post('/api/persons', (req, res) => personsApi.addContact(req, res))
+
+
+app.use(unknownEndpoint)
+
 
 const PORT = 3001
 
